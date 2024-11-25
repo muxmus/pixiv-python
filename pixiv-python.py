608f5320d6e361ef92b42b4206aabbda4342de39
木辛木杉 <1210718010@qq.com>
2024-11-26 01:35:05 +0800

    Update pixiv-python.py

--- pixiv-python.py
+++ pixiv-python.py
@@ -5,6 +5,7 @@ import json
 
 headers = {
     "cookie": "",    #若要抓取R-18和R-18G，需在此处填写cookie
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
     "referer": "https://www.pixiv.net/"
 }
 
